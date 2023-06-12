da6411f0c0ae63c183741d55c004e9dfa8476379
Tomasz Karpiej <tomash342@gmail.com>
2023-06-12 18:38:38 +0200

    Dodawanie projektu na GitHub

---
+++ src/main/java/com/zadanie/IT_Conference/ItConferenceApplication.java
@@ -0,0 +1,13 @@
+package com.zadanie.IT_Conference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItConferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItConferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zadanie/IT_Conference/ItConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zadanie.IT_Conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
