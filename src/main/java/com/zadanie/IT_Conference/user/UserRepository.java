997609507862e3a28ccb288de9e6ab498c2a797a
Tomasz Karpiej <tomash342@gmail.com>
2023-06-17 17:48:42 +0200

    Dodanie sprawdzania loginu (czy jest zajety) + wypisywanie komunikatu jezeli tak.

--- src/main/java/com/zadanie/IT_Conference/user/UserRepository.java
+++ src/main/java/com/zadanie/IT_Conference/user/UserRepository.java
@@ -8,6 +8,11 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    //szukanie użytkownika o podanym email
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     Optional<User> findUserByEmail(String email);
+
+    //szukanie użytkownika o podanym login
+    @Query("SELECT u FROM User u WHERE u.login = ?1")
+    Optional<User> findUserByLogin(String login);
 }
--- src/main/java/com/zadanie/IT_Conference/user/UserService.java
+++ src/main/java/com/zadanie/IT_Conference/user/UserService.java
@@ -19,21 +19,32 @@ public class UserService {
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
+
+    //wypisuje liste zarejestrowanych uzytkownikow ze wszystkimi danymi
     public List<User> getUsers(){
         return userRepository.findAll();
     }
 
+    //dodawanie nowego uzytkownika od bazy danych
     public void addNewUser(User user) {
-        Optional<User> userOptional = userRepository.findUserByEmail(user.getEmail());
-        if (userOptional.isPresent()){
+        Optional<User> userOptionalEmail = userRepository.findUserByEmail(user.getEmail());
+        //sprawdza, czy podany przez uzytkonwika mail nie jest zajety
+        if (userOptionalEmail.isPresent()){
             throw new IllegalStateException("Account with this mail is already created.");
+        }
+        //sprawdza, czy podany przez uzytkonwika login nie jest zajety
+        Optional<User> userOptionalLogin = userRepository.findUserByLogin(user.getLogin());
+        if (userOptionalLogin.isPresent()){
+            throw new IllegalStateException("Account with this login is already created.");
         }
         userRepository.save(user);
     }
 
-
+    //usuniecie uzytkownika
     public void deleteUser(Long userId) {
         userRepository.findById(userId);
+
+        //spradza, czy uzytkownik istnieje. Jezeli nie - wypisuje komunikat
         boolean exists = userRepository.existsById(userId);
         if (!exists){
             throw new IllegalStateException("User with id " + userId + " doesn't exists.");
@@ -41,11 +52,14 @@ public class UserService {
         userRepository.deleteById(userId);
     }
 
+    //aktualizowanie maila
     @Transactional
     public void updateUsersEmail(Long userId, String email) {
         User user = userRepository.findById(userId).orElseThrow(() ->
                 new IllegalStateException("User with id " + userId + " doesn't exists.")
         );
+
+        //sprawdza czy pole nie jest puste oraz czy nowy mail nie jest zajety
         if (email != null && email.length() > 0 && !Objects.equals(user.getEmail(), email)){
             Optional<User> userOptional = userRepository.findUserByEmail(email);
             if (userOptional.isPresent()) {
